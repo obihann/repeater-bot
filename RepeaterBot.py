ed5e3d7ca97481eef0d872ca004d0cac196a3977
Jeff Hann <jeff.hann@resmed.com>
2018-10-10 16:27:21 -0300

    starting

--- RepeaterBot.py
+++ RepeaterBot.py
@@ -1,9 +1,24 @@
 import httplib2
+import string
 from urllib import request
 from bs4 import BeautifulSoup
-from openpyxl import load_workbook
+from openpyxl import Workbook
 
 http = httplib2.Http()
+_KEYWORDS = [('Downlink', 'downlink'), 
+        ('Uplink', 'uplink'),
+        ('Offset', 'offset'),
+        ('Uplink Tone', 'uplinktone'),
+        ('Downlink Tone', 'downlinktone'),
+        ('Call', 'call'),
+        ('Use', 'use'),
+        ('Sponsor', 'sponsor'),
+        ('Affaliate', 'affaliate'),
+        ('Links', 'links'),
+        ('EchoLink', 'echolink'),
+        ('IRPL', 'irpl'),
+        ('Last update','lastupdate')]
+
 
 def search_rb(callsign):
     status, response = http.request("https://www.repeaterbook.com/repeaters/callResult.php?call=%s&submit=RepeaterBook" % callsign)
@@ -17,12 +32,6 @@ def search_rb(callsign):
     return links
 
 def load_rb(rb_href):
-    KEYWORDS = ['Downlink', 'Uplink', 'Offset', 
-            'Uplink Tone', 'Downlink Tone', 'Call', 
-            'Use', 'Sponsor', 'Affaliate', 
-            'Links', 'EchoLink', 'IRPL', 
-            'Last update']
-
     status, response = http.request("https://www.repeaterbook.com/repeaters/%s" % rb_href)
     soup = BeautifulSoup(response, 'html.parser')
 
@@ -31,38 +40,45 @@ def load_rb(rb_href):
     for x in soup.find_all('td'):
         rb_cell = str(x.renderContents().strip())
 
-        for keyword in KEYWORDS:
-            if rb_cell.find(keyword) != -1 and x.find_next_sibling():
-                details[keyword] = x.find_next_sibling().renderContents().strip()
+        for keyword in _KEYWORDS:
+            if rb_cell.find(keyword[0]) != -1 and x.find_next_sibling():
+                details[keyword[1]] = x.find_next_sibling().renderContents().strip()
 
     return details
 
 def main():
+    wb = Workbook()
+    wb_dest = 'repeaters.xlsx'
+    ws = wb.active
+    ws.title = 'Repeaters'
+
     with open('repeaters.txt') as f:
         callsigns = f.readlines()
 
     callsigns = [x.strip() for x in callsigns]
+    row_num = 2
 
-    for x in callsigns:
-        for url in search_rb(x):
-            print(load_rb(url))
+    _LETTERS = string.ascii_uppercase
 
+    for word in _KEYWORDS:
+        ws["%s1" % _LETTERS[_KEYWORDS.index(word)]] = word[0]
 
-    # workbook = load_workbook('radio.xlsx')
-    # sheet = workbook['Repeaters']
-    # row_num = 2
-    # col_name = 'A'
-    # cell_pos = "%s%d" % (col_name, row_num)
-    # cell = sheet[cell_pos]
+    for sign in callsigns:
+        for url in search_rb(sign):
+            repeater_data = load_rb(url)
+            ws["A%d" % (row_num)] = sign
 
-    # repeaters = {}
+            for word in _KEYWORDS:
+                print(hasattr(repeater_data, word[1]))
+                print(word[1])
+                print(repeater_data[word[1]])
+                # if hasattr(repeater_data, word[1]):
+                    # print(repeater_data[word[1]])
+                    # ws["%s%d" % (_LETTERS[_KEYWORDS.index(word)], row_num)] = repeater_data[word[1]]
 
-    # while cell.value is not None:
-        # repeaters[cell.value] = { "links": search_rb(cell.value)}
+            row_num += 1
 
-        # row_num += 1
-        # cell_pos = "%s%d" % (col_name, row_num)
-        # cell = sheet[cell_pos]
+    wb.save(filename = wb_dest)
 
 
 main()
