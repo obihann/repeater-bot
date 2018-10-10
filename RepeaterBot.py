24a2be0c8b942b58a9a2803e06c96a0edbbb9318
Jeff Hann <jeff.hann@resmed.com>
2018-10-10 14:19:26 -0300

    loading and scraping data from repeaterbook

--- RepeaterBot.py
+++ RepeaterBot.py
@@ -1,19 +1,47 @@
+import httplib2
+from urllib import request
+from bs4 import BeautifulSoup
 from openpyxl import load_workbook
-workbook = load_workbook('radio.xlsx')
-sheet = workbook['Repeaters']
+
+http = httplib2.Http()
+KEYWORDS = ['Downlink', 'Uplink', 'Offset', 
+        'Uplink Tone', 'Downlink Tone', 'Call', 
+        'Use', 'Sponsor', 'Affaliate', 
+        'Links', 'EchoLink', 'IRPL', 
+        'Last update']
+
+def load_rb(cell):
+    status, response = http.request(cell.hyperlink.target)
+    soup = BeautifulSoup(response, 'html.parser')
+
+    details = {}
+
+    for x in soup.find_all('td'):
+        rb_cell = str(x.renderContents().strip())
+
+        for keyword in KEYWORDS:
+            if rb_cell.find(keyword) != -1 and x.find_next_sibling():
+                details[keyword] = x.find_next_sibling().renderContents().strip()
 
 
-row_num = 2
-col_name = 'A'
-cell_pos = "%s%d" % (col_name, row_num)
-cell = sheet[cell_pos]
-
-while cell.value is not None:
     print(cell.value)
+    print(details)
 
-    if cell.hyperlink:
-        print(cell.hyperlink.target)
-
-    row_num += 1
+def main():
+    workbook = load_workbook('radio.xlsx')
+    sheet = workbook['Repeaters']
+    row_num = 2
+    col_name = 'A'
     cell_pos = "%s%d" % (col_name, row_num)
     cell = sheet[cell_pos]
+
+    while cell.value is not None:
+        if cell.hyperlink:
+            load_rb(cell)
+
+        row_num += 1
+        cell_pos = "%s%d" % (col_name, row_num)
+        cell = sheet[cell_pos]
+
+
+main()
